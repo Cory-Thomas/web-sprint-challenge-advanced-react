7aa6fd971b83f8b34f9984543b5ddf8a5dd0ee5f
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-09-04 11:44:12 -0400

    added a custom hook to form

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,15 @@
 // write your custom hook here to control your checkout form
+import { useLocalstorage } from './useLocalStorage';
+
+export const useForm = ( key, initialValue ) => {
+  const [ values, setValues ] = useLocalstorage( key, initialValue );
+
+  const handleChanges = event => {
+    setValues({
+      ...values,
+      [ event.target.name ]: event.target.value
+    });
+  };
+
+  return [ values, handleChanges ];
+};
